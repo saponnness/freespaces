3b4d88d9d01a769584bf05f3d038930060d8adf3
Saponchet Warakijsathapohn <saponchet.w@gmail.com>
2025-08-12 22:18:11 +0700

    Create authentication templates and URL routing.

--- freespaces/settings.py
+++ freespaces/settings.py
@@ -131,6 +131,11 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# Login/Logout redirects
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
