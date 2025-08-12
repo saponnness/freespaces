f2eb6b1963b0a6b02ed7fbfd1b8514c63b56f286
Saponchet Warakijsathapohn <saponchet.w@gmail.com>
2025-08-12 20:13:22 +0700

    Create authentication views and forms.

---
+++ accounts/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Profile
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(max_length=30, required=True)
+    last_name = forms.CharField(max_length=30, required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Add Tailwind CSS classes
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('bio', 'avatar')
+        widgets = {
+            'bio': forms.Textarea(attrs={
+                'class': 'w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500',
+                'rows': 4
+            }),
+            'avatar': forms.FileInput(attrs={
+                'class': 'w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
+            })
+        }
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib import messages
+from .forms import CustomUserCreationForm, ProfileUpdateForm
+from .models import Profile
 
 # Create your views here.
+def register(request):
+    """User registration view"""
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # Create profile for new user
+            Profile.objects.create(user=user)
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}! You can now log in.')
+            return redirect('accounts:login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+@login_required
+def profile(request, username=None):
+    """User profile view"""
+    if username:
+        user = get_object_or_404(User, username=username)
+    else:
+        user = request.user
+    
+    profile, created = Profile.objects.get_or_create(user=user)
+    return render(request, 'accounts/profile.html', {'profile': profile})
+
+@login_required
+def edit_profile(request):
+    """Edit user profile"""
+    profile, created = Profile.objects.get_or_create(user=request.user)
+    
+    if request.method == 'POST':
+        form = ProfileUpdateForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your profile has been updated!')
+            return redirect('accounts:profile')
+    else:
+        form = ProfileUpdateForm(instance=profile)
+    
+    return render(request, 'accounts/edit_profile.html', {'form': form})
