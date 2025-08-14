2d871e6c67c6e647607902a47b23d43ed6540615
Saponchet Warakijsathapohn <saponchet.w@gmail.com>
2025-08-14 13:18:00 +0700

    Register Post and Category models in the admin interface with custom display and search options.

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Post, Category
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'created_at')
+    search_fields = ('name',)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'category', 'status', 'created_at', 'published_at')
+    list_filter = ('status', 'category', 'created_at')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {}
+    
+    def save_model(self, request, obj, form, change):
+        if not change:  # If creating new post
+            obj.author = request.user
+        super().save_model(request, obj, form, change)
